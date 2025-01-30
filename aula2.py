5662be81a2a2ec84719120ee04587a184d4a0d15
Renan Gabriel Pereira Baptista <rbaptista@codin.gov>
2025-01-30 16:22:44 -0300

    Initial commit

---
+++ aula2.py
@@ -0,0 +1,19 @@
+nome_avo = 'Mirian'
+nome_mae = 'Karina'
+nome_pai = 'Renan'
+nome_filho = 'Miguel'
+nome_cachorro = 'Princesa'
+
+
+print ('Quem é a sua avó?', nome_avo)
+print ('Quem é a sua mãe?', nome_mae)
+print ('Quem é o seu pai?', nome_pai)
+print ('Quem é a seu filho?',type, nome_filho)
+print ('Quem é a sua cachorra?', nome_cachorro)
+
+
+
+
+
+
+
---
+++ aula4.py
@@ -0,0 +1,8 @@
+concatenacao = 'Renan' +' '+ 'Gabriel'
+print (concatenacao) 
+
+dez_vezes = 'A' * 10
+vinte_vezes = 3 * 'Luiz'
+
+print (dez_vezes)
+print (vinte_vezes) 
