314b9bb40d26a31e9a1b1e9a921ade41925c326b
Renan Gabriel Pereira Baptista <rbaptista@codin.gov>
2025-02-04 16:27:59 -0300

    terceiro commit

--- aula10.py
+++ aula10.py
@@ -1,17 +1,24 @@
 #if/elif/else
 #se/se não se/se não
 
-condicao = False
+condicao1 = False
+condicao2 = False
+condicao3 = False
+condicao4 = True
 
-if condicao: 
-    print('este codigo é o do if')
+if condicao1:
+    print('Código para condição 1')
+elif condicao2:
+    print('Código para condição 2')
+elif condicao3:
+    print('Código para condição 3')
+elif condicao4: 
+    print('Código para condição 4') 
 else:
-    print('primeiro if')
+    print('Nenhuma condição foi satisfeita')
 
-
-if 10 == 10:
+if 10 == 10.1:
     print ('outro if')
 
     
 print ('fora da linha')
-
---
+++ aula12.py
@@ -0,0 +1,10 @@
+primeiro_valor = input('Digite um valor: ')
+segundo_valor = input('digite outro valor: ')
+
+if primeiro_valor < segundo_valor:
+    print(f'{primeiro_valor=} é menor que o {segundo_valor=}')
+elif primeiro_valor > segundo_valor:
+    print(f'{primeiro_valor=} é maior que o {segundo_valor=}') 
+
+
+
